f070ca9056f31010fefc32a763cf574c409ad976
ZYP <zyp94021@gmail.com>
2019-04-15 15:02:04 +0800

    update eosplayer node

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './App.js'
 import zhCN from 'antd/lib/locale-provider/zh_CN'
 import { LocaleProvider } from 'antd'
-const eosplayer=window.eosplayer
-eosplayer.setNetConf("eosnode", {
+const eosplayer = window.eosplayer
+eosplayer.setNetConf('eosnode', {
   blockchain: 'eos',
-  host: "proxy.eosnode.tools",
+  host: 'proxy.eosnode.tools',
   port: 443,
-  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
   protocol: 'https',
-  httpEndpoint: "https://proxy.eosnode.tools:443",
+  httpEndpoint: 'https://proxy.eosnode.tools:443',
 })
-eosplayer.switchNetwork("eosnode");
+if (process.env.NODE_ENV === 'production') {
+  eosplayer.switchNetwork('eosnode')
+} else {
+  eosplayer.switchNetwork('dev')
+}
 ReactDOM.render(
   <LocaleProvider locale={zhCN}>
     <App />
